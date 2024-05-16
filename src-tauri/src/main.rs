a94b4c9f1c926ae0ed60879768ca3a68a6d6d387
Goldy <66202304+THEGOLDENPRO@users.noreply.github.com>
2024-05-16 21:19:03 +0100

    feat: remove dist directory, update cirrus and tauri configs.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,13 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri_plugin_theme::ThemePlugin;
-
 fn main() {
-    let mut ctx = tauri::generate_context!();
-
     tauri::Builder::default()
-        .plugin(ThemePlugin::init(ctx.config_mut()))
+        .plugin(cirrus_tauri::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React from "react";
+import { useEffect } from "react";
+
+import { initWindow } from "../cirrus/tauri_typescript";
 
 function App() {
+    useEffect(() => initWindow(), []);
+
     return (
         <div className="top-1/2 left-1/2 absolute -translate-y-1/2 -translate-x-1/2">
             <h1 className="text-center text-8xl font-bold mb-3">Cloudy</h1>
