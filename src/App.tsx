f711854f203ce61fd8238eef410fb5fc1dfb46cf
Goldy <66202304+THEGOLDENPRO@users.noreply.github.com>
2024-05-23 01:56:04 +0100

    feat, fix: change light mode text colour, light mode not working and add comment for init window.

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ import { useEffect } from "react";
 import { initWindow } from "../cirrus/tauri_typescript";
 
 function App() {
-    useEffect(() => initWindow(), []);
+    useEffect(() => initWindow(), []); // Loads the tauri window when the app component is mounted.
 
     return (
         <div className="top-1/2 left-1/2 absolute -translate-y-1/2 -translate-x-1/2">
             <h1 className="text-center text-8xl font-bold mb-3">Cloudy</h1>
-            <h3 className="text-nowrap">A template we use to get started with desktop apps.</h3>
+            <h3 className="text-nowrap font-mono">A template we use to get started with desktop apps.</h3>
         </div>
     );
 }
